28bdf3895845ebcc25b68f6e0b7022a040ee4b09
Guilherme Livianu <guilherme.livianu@gmail.com>
2024-02-27 19:21:32 -0300

    carrosel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
+"use client";
 import CardLg from "@/components/CardLg";
 import CardSm from "@/components/CardSm";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/navigation";
 
 export default function Page() {
   return (
@@ -7,17 +18,44 @@ export default function Page() {
       <div className=" flex flex-col gap-8 bg-gradient-to-b from-blue-light to to-blue-hard p-10">
         {/* ÚLTIMAS NOTÍCIAS */}
         <section className="flex items-center justify-center">
-          <div className="flex flex-col justify-center bg-blue-hard rounded-xl shadow-md">
+          <div className="flex flex-col justify-center bg-blue-hard rounded-xl shadow-md max-w-7xl">
             <div className=" bg-blue-hardest px-12 py-8 rounded-t-xl">
               <h2 className=" font-bold text-5xl text-white">
                 Últimas notícias
               </h2>
             </div>
 
-            <div className="flex gap-4 p-12">
-              <CardSm></CardSm>
-              <CardSm></CardSm>
-              <CardSm></CardSm>
+            <div className="p-12">
+              <Swiper
+                className=" "
+                slidesPerView={3}
+                spaceBetween={16}
+                navigation={true}
+                autoplay={{ delay: 3000 }}
+                modules={[Navigation, Autoplay]}
+              >
+                <SwiperSlide>
+                  <CardSm></CardSm>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <CardSm></CardSm>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <CardSm></CardSm>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <CardSm></CardSm>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <CardSm></CardSm>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <CardSm></CardSm>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <CardSm></CardSm>
+                </SwiperSlide>
+              </Swiper>
             </div>
           </div>
         </section>
